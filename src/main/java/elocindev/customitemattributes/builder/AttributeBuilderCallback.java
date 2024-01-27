b1cf09bc2306c400f5eaa844c668680bb223c2aa
nvb-uy <almhak@proton.me>
2024-01-27 17:13:14 -0300

    fix(per slot not working, 1.1.0)

--- src/main/java/elocindev/customitemattributes/builder/AttributeBuilderCallback.java
+++ src/main/java/elocindev/customitemattributes/builder/AttributeBuilderCallback.java
@@ -19,38 +19,44 @@ import net.minecraft.nbt.NbtCompound;
 public class AttributeBuilderCallback {
     public static void register() {
         ModifyItemAttributeModifiersCallback.EVENT.register((stack, slot, builder) -> {
-
             for (ItemProperty item : CustomItemAttributes.CONFIG.items) {
                 if (item.getItem() != stack.getItem()) continue;
+
+                NbtCompound nbt = stack.getNbt();
+                if (nbt != null && nbt.getBoolean("Unbreakable") != item.unbreakable) {
+                    nbt.putBoolean("Unbreakable", item.unbreakable);
+                    stack.writeNbt(nbt);
+                }
 
                 switch(slot) {
                     case MAINHAND:
                         applyModifiers(stack, item, item.overrides_main_hand, builder);
+                        break;
                     case OFFHAND:
                         applyModifiers(stack, item, item.overrides_off_hand, builder);
+                        break;
                     case HEAD:
                         applyModifiers(stack, item, item.overrides_head, builder);
+                        break;
                     case CHEST:
                         applyModifiers(stack, item, item.overrides_chest, builder);
+                        break;
                     case LEGS:
                         applyModifiers(stack, item, item.overrides_legs, builder);
+                        break;
                     case FEET:
                         applyModifiers(stack, item, item.overrides_feet, builder);
+                        break;
                 }
-
             }
         });
     }
 
-    public static void applyModifiers(ItemStack stack, ItemProperty property, List<GenericAttribute<String, ?>> attributes, Multimap<EntityAttribute, EntityAttributeModifier> builder) {
+    public static void applyModifiers(ItemStack stack, ItemProperty property, List<GenericAttribute<String, ?>> attributes, Multimap<EntityAttribute, EntityAttributeModifier> builder) {        
         for (GenericAttribute<?, ?> generic_attribute : attributes) {
             try {
                 EntityAttribute attribute = generic_attribute.getAttribute();
-                Collection<EntityAttributeModifier> newModifiers = new ArrayList<>();
-    
-                NbtCompound nbt = stack.getOrCreateNbt();
-                nbt.putBoolean("Unbreakable", property.unbreakable);
-                stack.writeNbt(nbt);
+                Collection<EntityAttributeModifier> newModifiers = new ArrayList<>();              
 
                 newModifiers.add(
                     new EntityAttributeModifier(
